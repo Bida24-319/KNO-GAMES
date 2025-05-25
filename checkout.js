8e82e95a12bc158c7c9b9e5a0fe6e9acee91e346
Bida24-319 <Bida24-319@thuto.bac.ac.bw>
2025-05-25 02:29:15 +0200

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
   let total = 0;
   cartItems.forEach(item => {
     const li = document.createElement("li");
-    li.textContent = `${item.product} - ${item.price.toFixed(2)}`;
+    li.textContent = `P${item.product} - P${item.price.toFixed(2)}`;
     cartList.appendChild(li);
     total += item.price;
   });
 
-  totalElement.textContent = `${total.toFixed(2)}`;
+  totalElement.textContent = `P${total.toFixed(2)}`;
 });
 
 document.getElementById("checkout-form").addEventListener("submit", function (e) {
